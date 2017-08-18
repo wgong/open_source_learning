4355095a08726aa4920725695df84c3247576d30
wgong <wen.g.gong@gmail.com>
2017-08-17 21:56:27 -0400

    Update perf.py

--- learn_stem/python/utilities/perf.py
+++ learn_stem/python/utilities/perf.py
@@ -5,4 +5,9 @@ print(timeit.timeit('for n in range(1000): n*n*n', number=10000))
 toshiba laptop:
 6.5739352509028635
 2.1101508438248997
+
+ubuntu desktop1:
+4.1188093860000095
+1.3006092190000231
+
 """
