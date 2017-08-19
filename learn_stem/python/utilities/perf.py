deac0e1ede44650686d3cc69a7ac9e3fd9e8d4ed
wgong <wen.g.gong@gmail.com>
2017-08-19 12:55:36 -0400

    Update perf.py

--- learn_stem/python/utilities/perf.py
+++ learn_stem/python/utilities/perf.py
@@ -6,8 +6,13 @@ toshiba laptop:
 6.5739352509028635
 2.1101508438248997
 
-ubuntu desktop1:
+ubuntu desktop-1:
 4.1188093860000095
 1.3006092190000231
+
+ubuntu desktop-2:
+/usr/bin/python perf.py # 2.7.12
+1.54211211205
+0.429780960083
 
 """
